4ffd691cfb4182e8812ba43eed1aaf38923c585e
Arjun Raj <87035926+AR4152@users.noreply.github.com>
2025-06-29 13:03:52 +1000

    remove text to improve readability

--- configure.sh
+++ configure.sh
@@ -1,32 +1,7 @@
-#!/bin/bash
-#
-# MIT License
-# 
-# Copyright (c) 2025 Arjun Raj
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# -----------------------------------------------------------------------------
 # Script Name: configure.sh
-# Description: 
+# Description:
 #   This script performs the necessary installation for the health_rag MLHub package.
-#   It checks if Ollama is installed on the system. If not, it prompts the user for 
+#   It checks if Ollama is installed on the system. If not, it prompts the user for
 #   permission to install it. Upon successful installation, it downloads the LLaMA 3.2 1B
 #   and nomic-embed-text models using Ollama.
 #
